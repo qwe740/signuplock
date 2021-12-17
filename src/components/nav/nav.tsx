140b92f43c4e714670ac1392f3d3baef12ee1e15
Austin Bervig <austinberviginc@gmail.com>
2021-12-16 21:16:14 -0600

    Setup

---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    render() {
+        return(
+            <div className="Nav">
+                <a href="#">Home</a>
+                <a href="#">Calculations</a>
+                <a href="#">Odds</a>
+            </div>
+        )
+    }
+}
+
+export default Nav;
