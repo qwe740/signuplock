8839de0116cd7a8496f283fd5ebfcfaffc97d483
Austin Bervig <austinberviginc@gmail.com>
2021-12-27 11:59:09 -0600

    A few additions

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './nav.css';
 class Nav extends React.Component {
     render() {
         return(
             <div className="Nav">
-                <a href="#">Home</a>
-                <a href="#">Calculations</a>
-                <a href="#">Odds</a>
+                <a href="#" className="nav-link">Home</a>
+                <a href="#" className="nav-link">Calculations</a>
+                <a href="#" className="nav-link">Odds</a>
             </div>
         )
     }
